45e184ccd30ec12bf9701d69c4088ec7b3d2c527
Bayla Angelo <111470903+PorthWenny@users.noreply.github.com>
2022-11-28 22:57:06 +0800

    Update StatsCalculator.c

--- StatsCalculator.c
+++ StatsCalculator.c
@@ -1,145 +1,149 @@
-#include <math.h>  // only for using "sqrt" smh...
-#include <stdio.h>
+#include<stdio.h>
+#include <math.h> // only for using "sqrt" smh...
 
-int main() {
-  int num[50], stop = 0, x[50];
-  int high;
-  float sum;
+int main(){
 
-  printf("== STATISTICS CALCULATOR (for GEMATH) ==\n");
-  printf(" Enter '0' to end the loop.\n\n");
-  for (int i = 0; i < 100; i++) {
-    printf(" Enter number: ");
-    scanf("%d", &num[i]);
+int num[50],stop=0, x[50];
+int high;
+float sum;
 
-    x[i] = num[i];
+printf("== STATISTICS CALCULATOR (for GEMATH) ==\n");
+printf(" Enter '0' to end the loop.\n\n");
+for(int i=0;i<100;i++){
+printf(" Enter number: ");
+scanf("%d", &num[i]);
 
-    if (num[i] > high) high = num[i];
+x[i] = num[i];
 
-    if (num[i] == 0) {
-      stop = i;
-      printf("\n Your set have %d element/s in it.", i);
-      break;
+if (num[i]>high)
+    high=num[i];
+
+if(num[i]==0){
+    stop=i;
+    printf("\n Your set have %d element/s in it.", i);
+    break;
     }
 
-    sum = sum + num[i];
-  }
+sum = sum + num[i];
+}
 
-  printf("\n YOUR SET: [ ");
-  for (int i = 0; i < stop; i++) {
-    for (int k = i + 1; k < stop; k++) {
-      if (num[k] < num[i]) {
-        int emp = num[i];
-        num[i] = num[k];
-        num[k] = emp;
+printf("\n YOUR SET: [ ");
+for(int i=0; i<stop; i++){
+          for(int k=i+1; k<stop;k++){
+            if(num[k] < num[i]){
+                int emp = num[i];
+                num[i] = num[k];
+                num[k] = emp;
+        }
+     }
+      printf("%d ", num[i]);
+    }
+printf("]\n");
+
+// GETTING THE MEAN
+float mean = sum/stop;
+printf("\n Mean = %g / %d = %.2f", sum, stop, mean);
+
+// GETTING THE MEDIAN
+printf("\n Median = ");
+
+int mid = stop/2;
+
+float center = num[mid] + num[mid-1];
+float median = center / 2;
+
+if(stop%2!=0)
+printf("Number %d = %d", mid+1, num[mid]);
+else
+printf("%d + %d = %d / 2 = %.1f", num[mid-1], num[mid], num[mid]+num[mid-1], median);
+
+// GETTING THE MODE
+printf("\n Mode = ");
+
+int max = 0, hold[20], count=0, k=0;
+
+for (int i = 0; i < stop; i++){
+        int mode = 0;
+
+        for (int j = i + 1; j < stop; j++){
+            if (num[i] == num[j]) {
+                mode++;
+            }
+        }
+        if ((mode > max) && (mode != 0)) {
+            k = 0;
+            max = mode;
+            hold[k] = num[i];
+            k++;
+        }
+        else if (mode == max) {
+            hold[k] = num[i];
+            k++;
+        }
+    }
+
+    for (int i = 0; i < stop; i++){
+        if (num[i] == hold[i])
+            count++;
+        }
+
+    if (count == stop)
+        printf("DNE.");
+    else{
+      for (int i = 0; i < k; i++)
+        printf("%d ", hold[i]);
       }
-    }
-    printf("%d ", num[i]);
-  }
-  printf("]\n");
 
-  // GETTING THE MEAN
-  float mean = sum / stop;
-  printf("\n Mean = %g / %d = %.2f", sum, stop, mean);
+// GETTING THE RANGE
+int range = num[stop-1] - num[0];
+printf ("\n Range = %d - %d = %d", num[stop-1], num[0], range);
 
-  // GETTING THE MEDIAN
-  printf("\n Median = ");
+char what;
+printf("\n\nContinue? [Y/n] ");
+scanf("%s", &what);
 
-  int mid = stop / 2;
+switch (what){
+	case 'N':
+	case 'n':
+		printf("\nGood job! Mr. Mojares would be happy.");
+		return 0;
+	case 'Y':
+	case 'y':
+		break;
+	default:
+		printf("Invalid input. Automatically exiting...");
+		return 0;
+}
 
-  float center = num[mid] + num[mid - 1];
-  float median = center / 2;
+printf("\nSAMPLE STANDARD DEVIATION CALCULATOR:\n");
+printf("[===================================================]\n");
+printf("[     x     ][     x - %g     ][      (x - %g)^2      ]\n", mean, mean);
 
-  if (stop % 2 != 0)
-    printf("Number %d = %d", mid + 1, num[mid]);
-  else
-    printf("%d + %d = %d / 2 = %.1f", num[mid - 1], num[mid],
-           num[mid] + num[mid - 1], median);
+double summ = 0;
 
-  // GETTING THE MODE
-  printf("\n Mode = ");
+for (int i=0;i<stop;i++){
 
-  int max = 0, hold[20], count = 0, k = 0;
+	int value = x[i] - mean;
+	int sqrd = value * value;
 
-  for (int i = 0; i < stop; i++) {
-    int mode = 0;
+	printf("  %d\t\t", x[i]);
 
-    for (int j = i + 1; j < stop; j++) {
-      if (num[i] == num[j]) {
-        mode++;
-      }
-    }
-    if ((mode > max) && (mode != 0)) {
-      k = 0;
-      max = mode;
-      hold[k] = num[i];
-      k++;
-    } else if (mode == max) {
-      hold[k] = num[i];
-      k++;
-    }
-  }
+	printf("%d\t\t", value);
 
-  for (int i = 0; i < stop; i++) {
-    if (num[i] == hold[i]) count++;
-  }
+	printf(" %d\t\t\t\n", sqrd);
 
-  if (count == stop)
-    printf("DNE.");
-  else {
-    for (int i = 0; i < k; i++) printf("%d ", hold[i]);
-  }
+	summ = summ + sqrd;
+}
 
-  // GETTING THE RANGE
-  int range = num[stop - 1] - num[0];
-  printf("\n Range = %d - %d = %d", num[stop - 1], num[0], range);
+float var = summ / (stop - 1);
+double ssd = sqrt(var);
 
-  char what;
-  printf("\n\nContinue? [Y/n] ");
-  scanf("%s", &what);
+	printf("[===================================================]\n");
+	printf("               TOTAL  =  %g \n", summ);
+	printf("	VARIANCE  =  TOTAL / %d  =  %.2f \n", stop-1, var);
+	printf("   STANDARD DEVIATION  =  sqrt(VAR)  = %.2lf  \n", ssd);
+	printf("[===================================================]\n");
 
-  switch (what) {
-    case 'N':
-    case 'n':
-      printf("\nGood job! Mr. Mojares would be happy.");
-      return 0;
-    case 'Y':
-    case 'y':
-      break;
-    default:
-      printf("Invalid input. Automatically exiting...");
-      return 0;
-  }
 
-  // SSD ONLY , NOT FOR POPULATION SD.
-  printf("\nSAMPLE STANDARD DEVIATION CALCULATOR:\n");
-  printf("[===================================================]\n");
-  printf("[     x     ][     x - %g     ][      (x - %g)^2      ]\n", mean,
-         mean);
-
-  double summ = 0;
-
-  for (int i = 0; i < stop; i++) {
-    int value = x[i] - mean;
-    int sqrd = value * value;
-
-    printf("  %d\t\t", x[i]);
-
-    printf("%d\t\t", value);
-
-    printf(" %d\t\t\t\n", sqrd);
-
-    summ = summ + sqrd;
-  }
-
-  float var = summ / (stop - 1);
-  double ssd = sqrt(var);
-
-  printf("[===================================================]\n");
-  printf("               TOTAL  =  %g \n", summ);
-  printf("	VARIANCE  =  TOTAL / %d  =  %.2f \n", stop - 1, var);
-  printf("   STANDARD DEVIATION  =  sqrt(VAR)  = %.2lf  \n", ssd);
-  printf("[===================================================]\n");
-
-  return 0;
+return 0;
 }
