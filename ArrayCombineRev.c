50bcd70254efa219506588058e2706a0403c9191
Bayla Angelo <111470903+PorthWenny@users.noreply.github.com>
2022-11-30 19:38:07 +0800

    Update ArrayCombineRev.c

--- ArrayCombineRev.c
+++ ArrayCombineRev.c
@@ -1,42 +1,48 @@
 #include<stdio.h>
 
 int main(){
-    
+
     int lim;
-    printf("Input the number of elements to be stored in the first array :");
+    printf("Input the number of elements to be stored in the first array: ");
     scanf("%d",&lim);
 
-    int num[100];
+    int num[lim];
     for(int i=0;i<lim;i++){
-         printf("element - %d : ", i);
+         printf("element - %d : ", i+1);
         scanf("%d",&num[i]);
     }
 
     int lim2;
-    printf("Input the number of elements to be stored in the second array :");
+    printf("Input the number of elements to be stored in the second array: ");
     scanf("%d",&lim2);
 
-    for(int i=0;i<lim2;i++){
-         printf("element - %d : ", i);
-        scanf("%d",&num[i+lim]);
+    int lim3=lim+lim2;
+    int combo[lim3];
+
+    for(int i=0;i<lim;i++){
+        combo[i]=num[i];
     }
 
-    int lim3=lim+lim2, ph[50];
+    for(int i=0;i<lim2;i++){
+        printf("element - %d : ", i+1);
+        scanf("%d",&combo[i+lim]);
+    }
 
      for(int i=0;i<lim3;i++){
          for(int k=i+1;k<lim3;k++){
-             if(num[k]>num[i])
+             if(combo[k]>combo[i])
              {
-                 ph[i]=num[k];
-                 num[k]=num[i];
-                 num[i]=ph[i];
+                 int ph[lim3];
+                 ph[i]=combo[k];
+                 combo[k]=combo[i];
+                 combo[i]=ph[i];
              }
          }
      }
 
     printf("\nThe merged array in decending order is :\n");
     for(int i=0;i<lim3;i++){
-         printf("%d ", num[i]);
+         printf("%d ", combo[i]);
         }
 
 return 0;
