2e19797cb53635c27a211e72ca64471b90003afd
Bayla Angelo <111470903+PorthWenny@users.noreply.github.com>
2022-12-05 00:54:33 +0800

    Greenlighted.

--- ArrayRepeatFinder.c
+++ ArrayRepeatFinder.c
@@ -4,7 +4,7 @@
 int main() {
     // set initial limit.
     int arr[1000000], limit;
-    
+
     // scan for elements in array with space acting as new element.
     printf("The given array is: ");
      do {
@@ -14,7 +14,7 @@ int main() {
 
     // set new limit to be dynamic.
     arr[limit];
-    
+
     // sort first to easier locate the already calculated repeaters.
     for(int i = 0; i < limit; i++) {
         for (int k = i + 1; k < limit; k++) {
@@ -26,9 +26,9 @@ int main() {
             }
         }
     }
-    
+
     // find how much an element repeated.
-    int repeat;
+    int repeat, none;
     for (int i = 0; i < limit; i++) {
         int count = 1;
         for (int k = i + 1; k < limit; k++) {
@@ -41,9 +41,14 @@ int main() {
         }
         if (count > 1)
             printf("The element '%d' occured %d times.\n",arr[i],count);
+        else if (count == 1)
+            none++;   
         repeat = arr[i];
     }
-
-
+    
+    // no repeat element if the number of "1 counts" are equal to the size of array.
+    if(none == limit)
+        printf("No repeating element was found.");
+   
 return 0;
 }
