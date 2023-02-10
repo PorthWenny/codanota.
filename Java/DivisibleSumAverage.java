159f97b13fbabe8e34dede032ed6417f1d70ae9f
Bayla Angelo <111470903+PorthWenny@users.noreply.github.com>
2023-02-10 16:45:45 +0800

    Create DivisibleSumAverage.java

---
+++ Java/DivisibleSumAverage.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.*;
+
+public class DivisibleSumAverage {
+  public static void main(String args[]) {
+    
+      System.out.println("The first 10 whole numbers divisible by 4 are: ");
+      int limit = 0, sum = 0;
+      
+      for (int i = 1; i > 0; i++) {
+          // limits the printing to 10 whole numbers only.
+          if (limit == 10) {
+              break;
+          }
+
+          // checks if 'i' value is divisible by 4, then run the if statements.
+          if (i % 4 == 0) {
+              System.out.println(i);
+
+              sum = sum + i;
+              limit++;
+          }
+      }
+
+       System.out.println("The sum is: "+sum);
+      int avg = sum/limit;
+      System.out.println("The average is: "+avg);
+
+    }
+}
