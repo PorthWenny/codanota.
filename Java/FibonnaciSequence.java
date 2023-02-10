6cf8f5b049799d874b854b312b81fbb0c696b690
Bayla Angelo <111470903+PorthWenny@users.noreply.github.com>
2023-02-10 17:29:53 +0800

    Create FibonnaciSequence.java

---
+++ Java/FibonnaciSequence.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+
+public class FibonacciSequence {
+  public static void main(String args[]) {
+    
+      Scanner holabels = new Scanner(System.in);
+
+      // gets input.
+      System.out.print("Enter a number to create a Fibonacci sequence: ");
+      int num = holabels.nextInt();
+
+      // initialize variables needed.
+      System.out.print("The Fibonacci sequence is: 0 1 ");
+
+      int fibo1 = 0, fibo2 = 1, fibo3 = 101010101;
+      for (int i = 0; i <= num-3; i++) {
+          fibo3 = fibo1 + fibo2;
+          System.out.print(fibo3+" ");
+
+          fibo1 = fibo2;
+          fibo2 = fibo3;
+      }
+      
+    }
+}
