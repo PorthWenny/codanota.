1a02636fb55b86fcd3049f296c5cb1f18aef3e2d
Bayla Angelo <abayla88@gmail.com>
2023-02-11 12:56:32 +0000

    Commited from VSC, done.

---
+++ Java/PrintAsteriskRows.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+public class PrintAsteriskRows {
+    public static void main(String[] kwentoakizkiz) {
+        try (Scanner holabels = new Scanner(System.in)) {
+            int row;
+      
+            System.out.print("Input the number of rows: ");
+            row = holabels.nextInt();
+
+            for (int i = 1; i <= row; i++) {
+                for (int j = 1; j <= 10; j++) {
+                    System.out.print("*");
+                }
+                System.out.println("");
+            }
+        }
+
+    }
+
+}
