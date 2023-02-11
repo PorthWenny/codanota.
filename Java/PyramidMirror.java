5f8d88450d1a4921603af161668292dd31655e51
Bayla Angelo <abayla88@gmail.com>
2023-02-11 14:19:06 +0000

    Edited errors in VSC.

--- Java/PyramidMirror.java
+++ Java/PyramidMirror.java
@@ -6,37 +6,32 @@ public class PyramidMirror {
         try (Scanner holabels = new Scanner(System.in)) {
             int row;
       
-     System.out.print("Input value of n: ");
+            System.out.print("Input value of n: ");
             row = holabels.nextInt();
 
-            // initialize variables to be used.
-            int space = row-1, placeholder = 1;
-            // main for loop
+            // main loop
             for (int i = 1; i <= row; i++) {
                 
                 // for spacing
-                for (int a = space; a >= 1; a--) {
+                for (int k = row; k >= i+1; k--) {
                     System.out.print(" ");
                 }
-               
-                int num = placeholder;
+
+                int num = i;
                 // for left side design
                 for (int j = 1; j <= i; j++) {
                     System.out.print(num);
                     num--;
                 }
 
-                int num2 = num+1;
                 //for right side design
                  if (i > 1) {
                     for (int j = 1; j < i; j++) {
-                        System.out.print(num2+1);
-                        num2++;
+                        System.out.print(num+2);
+                        num++;
                     }
                 }
-               
-                space--;
-                placeholder++;
+                
                 System.out.println(" ");
             }
         }
---
+++ Java/PyramidWithNumbers.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class PyramidWithNumbers {
+    public static void main(String[] coderakizkiz) {
+
+        try (Scanner holabels = new Scanner(System.in)) {
+            int row;
+      
+            System.out.print("Input the number of rows (or asterisks): ");
+            row = holabels.nextInt();
+
+            for (int i = 1; i <= row; i++) {
+                // loop for spacing.
+                for (int k = row; k >= i+1; k--) {
+                    System.out.print(" ");
+                }
+                // loop for asterisk (left).
+                for (int j = 1; j <= i; j++) {
+                    System.out.print(i);
+                }
+                // loop for asterisk (right).
+                if (i != 1) {
+                    for (int j = 2; j <= i; j++) {
+                        System.out.print(i);
+                    }
+                }
+                System.out.println(""); 
+            }
+        }
+    }
+}
