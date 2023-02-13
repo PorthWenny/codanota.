a229631450edc15e8e2d88b5b5b485d0356a6a5e
Bayla Angelo <abayla88@gmail.com>
2023-02-13 14:43:55 +0000

    just a short recap for me.

---
+++ Java/MultiplicationForLoop.java
@@ -0,0 +1,12 @@
+public class MultiplicationForLoop {
+    public static void main(String[] coderakizkiz) {
+        for (int i = 3; i <=36; i++) {
+            // checks if i is a multiple of 3, then prints.
+            if (i % 3 == 0) {
+            System.out.print(i+" ");
+            } 
+            /* always make it a habit to use curly brackets...
+            (even if the statement uses only one line.) */ 
+        }
+    }
+}
