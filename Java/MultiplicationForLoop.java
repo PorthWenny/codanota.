9623ee60e54005099965b0bd4c0e985c7e59139f
Bayla Angelo <abayla88@gmail.com>
2023-02-13 14:53:02 +0000

    short note for java looping...

--- Java/MultiplicationForLoop.java
+++ Java/MultiplicationForLoop.java
@@ -1,6 +1,6 @@
 public class MultiplicationForLoop {
     public static void main(String[] coderakizkiz) {
-        for (int i = 3; i <=36; i++) {
+        for (int i = 3; i <=38; i++) {
             // checks if i is a multiple of 3, then prints.
             if (i % 3 == 0) {
             System.out.print(i+" ");
