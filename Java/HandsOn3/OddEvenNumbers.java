bbd8f7404441357225f09bea958ffb331ab3aa29
Bayla Angelo <abayla88@gmail.com>
2023-02-16 23:57:28 +0000

    wow new file.

---
+++ Java/HandsOn3/OddEvenNumbers.java
@@ -0,0 +1,18 @@
+public class OddEvenNumbers {
+    public static main void (String[] coderakizkiz) {
+
+        int sum = 0;
+
+        System.out.println("The first 15 even numbers are: ");
+        for (int i = 0; i <= 30; i++) {
+            if (i % 2 == 0) {
+                System.out.println(i);
+            }
+            else {
+                sum = sum + i;
+            }
+        }
+
+        System.out.println("The sum of the first 15 odd numbers are: " +sum);
+    }
+}
