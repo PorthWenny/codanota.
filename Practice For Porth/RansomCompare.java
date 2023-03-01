e6c8bd291184465eaa21c3b49f4b0ae3a228384d
Bayla Angelo <111470903+PorthWenny@users.noreply.github.com>
2023-03-01 15:01:19 +0800

    Create RansomCompare.java

---
+++ Practice For Porth/RansomCompare.java
@@ -0,0 +1,39 @@
+import java.io.*;
+import java.util.*;
+
+class RansomCompare {
+    public static boolean canConstruct(String ransomNote, String magazine) {
+        int counter = 0;
+
+        outer:
+        for (int i = 0; i < ransomNote.length();i++) {
+            for (int j = 0; j < magazine.length();j++) {
+                if (ransomNote.charAt(i) == magazine.charAt(j)) {
+                    magazine = magazine.substring(0, j) + "" + magazine.substring(j+1);
+                    counter++;
+                    continue outer;
+                }
+             }
+        }
+
+        if (counter == ransomNote.length()) {
+            return true;
+        }
+        else
+            return false;
+        }
+
+    public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter ransom note: ");
+        String ransomNote = sc.nextLine();
+        System.out.print("Enter magazine: ");
+        String magazine = sc.nextLine();
+        boolean result = canConstruct(ransomNote, magazine);
+
+        canConstruct(ransomNote, magazine);
+
+        System.out.println("\n"+result);
+    }
+}
