a207826f787df3493e7e725fa917bb7ccb9dd22a
Bayla Angelo <abayla88@gmail.com>
2023-03-07 13:40:37 +0000

    hoes mad.

---
+++ Practice For Porth/LargestQuotient.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class LargestQuotient {
+    public static void main (Strings[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        ArrayList<Integer> numArr = new ArrayList<Integer>();
+
+        do {
+            numArr.add(input.nextInt());
+        } while (numArr[numArr.length-1] != 0);
+    }
+}
