30c4445f5cea6d4ae7b12a8b71f3a1da62324631
Bayla Angelo <abayla88@gmail.com>
2023-03-10 13:32:24 +0000

    new handson made in Gitpod.

---
+++ Java/3DArraySearch.java
@@ -0,0 +1,45 @@
+import java.util.*;
+
+public class 3DArraySearch {
+    public static void main (String[] args) {
+
+        try (Scanner input = new Scanner(System.in)) {
+            int[][][] numArr = new int[2][2][5];
+            int pos=0, neg=0, odd=0, even=0, zero=0-9;
+
+            System.out.println("Enter values of 3D array: ");
+            for (int i = 0; i < numArr.length; i++) {
+                for (int j = 0; j < numArr[i].length; j++) {
+                    for (int k = 0; k < numArr[i][j].length; k++) {
+                        numArr[i][j][k] = input.nextInt();
+
+                // for positive, negative, and zero counters.
+                if (numArr[i][j][k] > 0) {
+                    pos++;
+                }
+                else if (numArr[i][j][k] == 0) {
+                    zero++;
+                }
+                else {
+                    neg++;
+                }
+
+                // for odd or even counters.
+                if (numArr[i][j][k] % 2 == 0) {
+                    even++;
+                }
+                else {
+                    odd++;
+                }
+            }
+        }
+    }
+
+        System.out.println("Number of positive numbers: " + pos);
+        System.out.println("Number of negative numbers: " + neg);
+        System.out.println("Number of odd numbers: "+ odd);
+        System.out.println("Number of even numbers: "+ even);
+        System.out.println("Number of 0s: " + zero);
+        }
+    }
+}
---
+++ Java/BiggestSmallest.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class BiggestSmallest {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        int[][][] numArr = new int[3][2][3];
+        int high = numArr[0][0][0];
+        int small = numArr[0][0][0];
+
+        System.out.println("Enter the value of the 3-D array: ");
+
+        for (int i = 0; i < numArr.length; i++) {
+            for (int j = 0; j < numArr[i].length; j++) {
+                for (int k = 0; k < numArr[i][j].length; k++) {
+                    numArr[i][j][k] = input.nextInt();
+
+                    if (numArr[i][j][k] > high) {
+                        high = numArr[i][j][k];
+                    }
+                    
+                    if (numArr[i][j][k] < small) {
+                        small = numArr[i][j][k];
+                    }
+                }
+            }
+        }
+
+        System.out.println("The highest value is: " + high);
+        System.out.println("The smallest value is: " + small);
+    }
+}
