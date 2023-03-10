217186a0be750d45a656f9054319a34c9399ac72
Bayla Angelo <abayla88@gmail.com>
2023-03-10 17:38:18 +0000

    new file

--- Java/HandsOn1Mid/BiggestSmallestArray.java
+++ Java/HandsOn1Mid/BiggestSmallestArray.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class BiggestSmallest {
+public class BiggestSmallestArray {
     public static void main(String[] args) {
 
         Scanner input = new Scanner(System.in);
