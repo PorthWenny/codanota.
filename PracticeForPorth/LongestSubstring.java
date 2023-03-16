133a5cd46f20d309109ab2a55ccce33f6fc2cce3
Bayla Angelo <abayla88@gmail.com>
2023-03-16 15:10:49 +0000

    update working for VSC.

--- PracticeForPorth/LongestSubstring.java
+++ PracticeForPorth/LongestSubstring.java
@@ -1,40 +1,34 @@
 import java.util.*;
 
-public class LongestSubstring {
+public class LongestSubstring {   
+    public static int lengthOfLongestSubstring (String s) {
+        Set<Character> set = new HashSet();
+
+        int max = 0;
+        int left = 0;
+
+        /* starts a while loop that checks if the character at the current index 
+        is already in the set of characters. If it is, the loop removes the character 
+        at the left index of the substring from the set and increments left. */
+
+        for (int i = 0; i < s.length(); i++) {
+            while(!set.add(s.charAt(i))) {
+                set.remove(s.charAt(left++));
+            }
+            // update max value between max & substring length + 1
+            max = Math.max(max, right - left + 1);
+        }
+
+        return max;
+    }
+    
     public static void main (String[] hi) {
-        
+
         Scanner input = new Scanner(System.in);
         
         System.out.print("s = ");
-        String text = input.nextLine();
+        String s = input.nextLine();
 
-        int count = 0;
-        int lastc = 1;
-
-        for (int i = 0; i < text.length(); i++) {
-            for (int j = i+1; j < text.length(); j++) {
-                if (text.charAt(i) == text.charAt(j)) {
-                    text = text.substring(0, j) + "" + text.substring(j+1);
-                }
-            }
-        }
-
-
-        for (int i = 0; i < text.length(); i++) {
-            if (i+1 != text.length()) {
-                if (text.charAt(i) != text.charAt(i+1)) {
-                    count++;
-                    lastc = count;
-                    continue;
-                }
-                else {
-                    count = 0;
-                    continue;
-                }
-            }
-        }
-
-        System.out.println(lastc);
-
+        System.out.println(lengthOfLongestSubstring(s));
     }
 }
