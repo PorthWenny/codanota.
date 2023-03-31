ea616f3173b319b0b56ee375d40ce917e5ea07cf
Bayla Angelo <abayla88@gmail.com>
2023-03-31 02:09:35 +0000

    update errors.

--- Java/HandsOn3Mid/ArrayListMethodCountries.java
+++ Java/HandsOn3Mid/ArrayListMethodCountries.java
@@ -5,6 +5,14 @@ public class ArrayListMethodCountries {
         ArrayList<String> countries = new ArrayList<>(10);
 
         // add 9 String of countries for our ArrayList 
+        storeList(countries);
+
+        System.out.println("List of Countries");
+        // print out every values from the ArrayList with new line after each index
+        printList(countries);
+    }
+
+    static void  storeList (ArrayList<String> countries) {
         countries.add("Japan");
         countries.add("US");
         countries.add("Philippines");
@@ -14,8 +22,9 @@ public class ArrayListMethodCountries {
         countries.add("Hong Kong");
         countries.add("Malaysia");
         countries.add("Great Britain");
+    }
 
-        System.out.println("List of Countries");
+    static void printList (ArrayList<String> countries) {
         // print out every values from the ArrayList with new line after each index
         for (int i = 0; i < countries.size(); i++) {
             System.out.println(countries.get(i));
