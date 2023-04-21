a4a8739308e523e7767bc13665768bf342aad58e
Bayla Angelo <abayla88@gmail.com>
2023-04-21 06:53:35 +0000

    github push.

---
+++ Java/HandsOn1Finals/BasicStackOperations.java
@@ -0,0 +1,28 @@
+import java.util.*;
+import java.io.*;
+
+class BasicStackOperations {
+    public static void main (String[] args) {
+        Stack<Integer> nigerian = new Stack<>();
+
+        nigerian.push(67);
+        nigerian.push(12);
+        nigerian.push(3);
+        nigerian.push(4);
+        nigerian.push(5);
+        nigerian.push(6);
+        nigerian.push(7);
+        nigerian.push(8);
+        nigerian.push(67);
+        nigerian.push(89);
+
+        System.out.println("Original stack elements: " + nigerian);
+
+        nigerian.pop();
+        nigerian.pop();
+        nigerian.pop();
+        System.out.println("Stack after pop operations: " + nigerian);
+        System.out.println("The top element is: " + nigerian.peek());
+        System.out.println("Is stack empty? " + nigerian.isEmpty());
+    }
+}
